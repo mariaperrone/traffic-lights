e66b158fd2afb9ca14575a25adb6d24087b01bc3
mariaperrone <perronemar148@gmail.com>
2021-09-16 01:13:26 +0000

    in progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
+const TrafficLight = () => {
+	const [redlightOn, setRedligthOn] = useState(false);
+	const [yellowlightOn, setYellowligthOn] = useState(false);
+	const [greenlightOn, setGreenligthOn] = useState(false);
+	let redclass = "red-light bg-danger mt-3";
+	let yellowclass = "yellow-light bg-warning";
+	let greenclass = "green-light bg-success";
+
+	if (redlightOn == true) {
+		redclass += " selected";
+		yellowclass = "yellow-light bg-warning";
+		greenclass = "green-light bg-success";
+	}
+
+	if (yellowlightOn == true) {
+		redclass = "red-light bg-danger mt-3";
+		yellowclass += " selected";
+		greenclass = "green-light bg-success";
+	}
+
+	if (greenlightOn == true) {
+		redclass = "red-light bg-danger mt-3";
+		yellowclass = "yellow-light bg-warning";
+		greenclass += " selected";
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="cuerpo d-flex justify-content-center align-items-center">
+			<div className="container bg-dark justify-content-center">
+				<button
+					className={redclass}
+					onClick={() => setRedligthOn(true)}></button>
+				<button
+					className={yellowclass}
+					onClick={() => setYellowligthOn(true)}></button>
+				<button
+					className={greenclass}
+					onClick={() => setGreenligthOn(true)}></button>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default TrafficLight;
