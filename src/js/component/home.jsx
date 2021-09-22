c3ad53faa00437fd55a70e7ac47c329b50da3e3a
mariaperrone <perronemar148@gmail.com>
2021-09-22 20:30:12 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,39 +5,46 @@ const TrafficLight = () => {
 	const [redlightOn, setRedligthOn] = useState(false);
 	const [yellowlightOn, setYellowligthOn] = useState(false);
 	const [greenlightOn, setGreenligthOn] = useState(false);
+
 	let redclass = "red-light bg-danger mt-3";
 	let yellowclass = "yellow-light bg-warning";
 	let greenclass = "green-light bg-success";
 
-	if (redlightOn == true) {
-		redclass += " selected";
-		yellowclass = "yellow-light bg-warning";
-		greenclass = "green-light bg-success";
-	}
+	const turnRedLightOn = () => {
+		setRedligthOn(true);
+		setYellowligthOn(false);
+		setGreenligthOn(false);
+	};
 
-	if (yellowlightOn == true) {
-		redclass = "red-light bg-danger mt-3";
-		yellowclass += " selected";
-		greenclass = "green-light bg-success";
-	}
+	const turnYellowLightOn = () => {
+		setRedligthOn(false);
+		setYellowligthOn(true);
+		setGreenligthOn(false);
+	};
 
-	if (greenlightOn == true) {
-		redclass = "red-light bg-danger mt-3";
-		yellowclass = "yellow-light bg-warning";
-		greenclass += " selected";
-	}
+	const turnGreenLightOn = () => {
+		setRedligthOn(false);
+		setYellowligthOn(false);
+		setGreenligthOn(true);
+	};
+
 	return (
-		<div className="cuerpo d-flex justify-content-center align-items-center">
-			<div className="container bg-dark justify-content-center">
+		<div className="cuerpo">
+			<div className="bg-dark palo mx-auto row"></div>
+			<div className="container bg-dark row">
 				<button
-					className={redclass}
-					onClick={() => setRedligthOn(true)}></button>
+					className={redlightOn ? redclass + " selected" : redclass}
+					onClick={turnRedLightOn}></button>
 				<button
-					className={yellowclass}
-					onClick={() => setYellowligthOn(true)}></button>
+					className={
+						yellowlightOn ? yellowclass + " selected" : yellowclass
+					}
+					onClick={turnYellowLightOn}></button>
 				<button
-					className={greenclass}
-					onClick={() => setGreenligthOn(true)}></button>
+					className={
+						greenlightOn ? greenclass + " selected" : greenclass
+					}
+					onClick={turnGreenLightOn}></button>
 			</div>
 		</div>
 	);
